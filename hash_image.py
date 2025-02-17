925dec8cdb285b1ac72a3304633ef122117bbe79
Jerrin <jwofford1110@gmail.com>
2025-02-16 23:04:48 -0500

    added extracting image metadata (exif and base info)

--- hash_image.py
+++ hash_image.py
@@ -1,4 +1,7 @@
 import hashlib
+import time
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 def file_hash(file_path) -> str:
     """
@@ -15,3 +18,42 @@ def file_hash(file_path) -> str:
             md5.update(data)
 
     return md5.hexdigest()
+
+def extract_raw(file_path) -> ...:
+    """
+    Takes the data stored in an image file and
+    extracts it to be stored
+    """
+    image = Image.open(file_path)
+    image_info = {
+        "Filename": image.filename,
+        "Image Size": image.size,
+        "Image Height": image.height,
+        "Image Width": image.width,
+        "Image Format": image.format,
+        "Image Mode": image.mode,
+        "Image is Animatedd": getattr(image, "is_animated", False),
+        "Frames in Image": getattr(image, "n_frames", 1)
+    }
+
+def extract_exif(file_path) -> ...:
+    """
+    Takes the exif data stored in an image file
+    and extracts it to be stored
+    """
+    image = Image.open(file_path)
+    exif_data = image.getexif()
+
+    exif_info = {}
+    for tag_id in exif_data:
+        tag = TAGS.get(tag_id, tag_id)
+        data = exif_data.get(tag_id)
+        if isinstance(data, bytes):
+            data = data.decode()
+        exif_info[tag] = data
+        
+
+start = time.time()
+print(file_hash('cat_test.jpg'))
+end = time.time()
+print((end - start) * 10**3, 'ms')
