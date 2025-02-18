ec355906b076a7ab9a592624c5400f649c16d4a4
Jerrin <jwofford1110@gmail.com>
2025-02-18 15:30:00 -0500

    added util file

---
+++ tagsense/util.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module holds variables used by other modules.
+The data here can be edited, just be careful.
+"""
+
+# **** IMPORTS ****
+import logging
+from pathlib import Path
+
+# **** LOGGING ****
+# Sets up logger
+logger = logging.getLogger(__name__)
+
+
+
+# ****
+if __name__ == "__main__":
+    raise Exception("This file is not meant to run on its own.")
